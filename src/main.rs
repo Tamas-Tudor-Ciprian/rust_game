cce28226af23bf14707da5694562242593b1369a
Tudor <uiv80988@vitesco.com>
2026-02-02 09:32:26 +0200

    I forgot to add this commit last time, got borders now

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use std::time::Instant;
 
 
 
+const SCREEN_MEASURES: (i32,i32) = (60,30);
+
 #[derive(Clone)]
 enum ScreenPixel {
 
@@ -22,16 +24,12 @@ enum ScreenPixel {
 	Wall,
 	Empty,
 
-
-
 }
-
-
 
 struct Position{
 
-	x:i32,
-	y:i32,
+	x:f64,
+	y:f64,
 
 }
 
@@ -39,6 +37,7 @@ struct Position{
 
 struct Crab {
 	position : Position,
+	speed : f64,
 	emoji : char,
 
 }
@@ -47,11 +46,10 @@ impl Default for Crab{
 
 
 	fn default() -> Self{
-		Self{position: Position {x : 10, y: 20},
+		Self{position: Position {x : 10.0, y: 20.0},
+		speed : 0.25,
 		emoji:'🦀',
-
-
-		} }
+	} }
 }
 
 
@@ -64,7 +62,7 @@ fn display_framerate(out : &mut Stdout, start_time : &mut Instant){
 	
 	let mut time_now = Instant::now();
 
-	let elapsed_time = *start_time - time_now;
+	let elapsed_time = time_now - *start_time;
 
 
 	*start_time = time_now;
@@ -74,24 +72,44 @@ fn display_framerate(out : &mut Stdout, start_time : &mut Instant){
 
 	out.execute(cursor::MoveTo(0,0)).unwrap();
 
-	write!(out,"        ");
 
-
-	out.execute(cursor::MoveTo(0,0)).unwrap();
-
-
-	write!(out,"FPS:{}",fps);
-
-
-
+	write!(out,"FPS:{}",fps).unwrap();
 
 	}
 
 
+fn display_speed(out : &mut Stdout,crab :&Crab){
 
 
+	out.execute(cursor::MoveTo(SCREEN_MEASURES.0.try_into().unwrap(),25));
 
 
+	write!(out,"SPEED:{}",crab.speed).unwrap();
+}
+
+fn make_walls(out : &mut Stdout){
+
+
+	out.execute(cursor::MoveTo(0,1)).unwrap();
+	for _ in 0..SCREEN_MEASURES.0{
+		write!(out,"#").unwrap();
+	}
+
+	out.execute(cursor::MoveTo(0,SCREEN_MEASURES.1.try_into().unwrap())).unwrap();
+	for _ in 0..SCREEN_MEASURES.0{
+		write!(out,"#").unwrap();
+	}
+
+	for left_rail in 1..=SCREEN_MEASURES.1{	
+	
+		out.execute(cursor::MoveTo(0,left_rail.try_into().unwrap())).unwrap();	
+		write!(out,"#").unwrap();
+		out.execute(cursor::MoveTo(SCREEN_MEASURES.0.try_into().unwrap(),left_rail.try_into().unwrap())).unwrap();
+		write!(out,"#").unwrap();
+	
+	}
+		
+}
 
 
 
@@ -106,51 +124,47 @@ fn main(){
 
 	let mut screen_buffer  = vec![vec![ScreenPixel::Empty;collumns];rows];
 
-
-
 	let mut stdout = stdout();
 
 	terminal::enable_raw_mode().unwrap();
 	stdout.execute(terminal::EnterAlternateScreen).unwrap();
 	stdout.execute(terminal::Clear(ClearType::All)).unwrap();
 
-
-
 		sleep(Duration::from_millis(50));
-
-
-
-
 
 	let mut crab : Crab = Crab::default();
 
-	stdout.execute(cursor::MoveTo(crab.position.x.try_into().unwrap(),crab.position.y.try_into().unwrap())).unwrap();
+	stdout.execute(cursor::MoveTo(crab.position.x as u16,crab.position.y as u16)).unwrap();
 	write!(stdout,"{}",crab.emoji).unwrap();
 	stdout.flush().unwrap();
+
+
+	make_walls(&mut stdout);
 
 	loop{
 	// this is the main game loop
 
-
-	if event::poll(Duration::from_millis(500)).unwrap_or(false) {
+	if event::poll(Duration::from_millis(0)).unwrap_or(false) {
 		if let Ok(Event::Key(key)) = event::read() {
 			match key.code{
 				KeyCode::Char('q') => break,
 				KeyCode::Left => {
-					crab.position.x -= 1;
-					stdout.execute(cursor::MoveTo(crab.position.x.try_into().unwrap(),crab.position.y.try_into().unwrap())).unwrap();
+					crab.position.x -= crab.speed;
+					stdout.execute(cursor::MoveTo(crab.position.x as u16,crab.position.y as u16)).unwrap();
 
 					write!(stdout,"{}",crab.emoji).unwrap();
 					stdout.flush().unwrap();
 					},
 				KeyCode::Right => {
-					crab.position.x +=1;
-					stdout.execute(cursor::MoveTo(crab.position.x.try_into().unwrap(),crab.position.y.try_into().unwrap())).unwrap();
+					crab.position.x += crab.speed;
+					stdout.execute(cursor::MoveTo(crab.position.x as u16,crab.position.y as u16)).unwrap();
 					write!(stdout,"{}",crab.emoji).unwrap();
 					stdout.flush().unwrap();
 
 
 					},
+				KeyCode::Up => {},
+				KeyCode::Down => {},
 				_ => {},
 
 			}	
@@ -160,14 +174,18 @@ fn main(){
 	}
 
 		//I will attempt to have some logic to display the frame rate here
+		
+		let mut start_time = Instant::now();
 
+		display_framerate(&mut stdout,&mut start_time);
+		
 
 }
 
 
 
 
-	stdout.execute(cursor::Show).unwrap();
+//	stdout.execute(cursor::Show).unwrap();
 	stdout.execute(terminal::LeaveAlternateScreen).unwrap();
 	terminal::disable_raw_mode().unwrap();
 
